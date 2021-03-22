c2c8187b4363a8062b97538fab353324ad81f6e9
vmayv <vasyamayorov@yandex.ru>
2021-03-22 15:55:47 +0300

    Добавлено тело класс CrudController

--- WebApplication/Controllers/CrudController.cs
+++ WebApplication/Controllers/CrudController.cs
@@ -11,5 +11,43 @@ namespace WebApplication.Controllers
     [ApiController]
     public class CrudController : ControllerBase
     {
+        private readonly ValuesHolder holder;
+
+        public CrudController(ValuesHolder holder)
+        {
+            this.holder = holder;
+        }
+        [HttpPost("create")]
+        public IActionResult Create([FromQuery] string input)
+        {
+            holder.Values.Add(input);
+            return Ok();
+        }
+
+        [HttpGet("read")]
+        public IActionResult Read()
+        {
+            return Ok(holder.Values);
+        }
+
+        [HttpPut("update")]
+        public IActionResult Update([FromQuery] string stringsToUpdate, [FromQuery] string newValue)
+        {
+            for (int i = 0; i < holder.Values.Count; i++)
+            {
+                if (holder.Values[i] == stringsToUpdate)
+                    holder.Values[i] = newValue;
+            }
+
+            return Ok();
+        }
+
+        [HttpDelete("delete")]
+        public IActionResult Delete([FromQuery] string stringsToDelete)
+        {
+            holder.Values = holder.Values.Where(w => w != stringsToDelete).ToList();
+            return Ok();
+        }
+
     }
 }
---
+++ WebApplication/ValuesHolder.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication
+{
+    public class ValuesHolder
+    {
+        public List<string> Values { get; set; }
+
+        public ValuesHolder()
+        {
+            Values = new List<string>();
+        }
+    }
+}
