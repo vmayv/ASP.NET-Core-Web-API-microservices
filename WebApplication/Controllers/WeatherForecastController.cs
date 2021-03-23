fdedd53e75e59ecfedbf69661a0ed2bc6e695314
vmayv <vasyamayorov@yandex.ru>
2021-03-23 16:31:56 +0300

    Тип входных параметров типа Дата заменён на DateTime

--- WebApplication/Controllers/WeatherForecastController.cs
+++ WebApplication/Controllers/WeatherForecastController.cs
@@ -18,20 +18,20 @@ namespace WebApplication.Controllers
         }
 
         [HttpPost("save")]
-        public IActionResult Save([FromQuery] string date, [FromQuery] int temperatureC)
+        public IActionResult Save([FromQuery] DateTime date, [FromQuery] int temperatureC)
         {
-            _holder.Values.Add(new WeatherForecast(Convert.ToDateTime(date), temperatureC));
+            _holder.Values.Add(new WeatherForecast(date, temperatureC));
             return Ok();
         }
 
         [HttpGet("read")]
-        public IActionResult Read([FromQuery] string fromDate, [FromQuery] string toDate)
+        public IActionResult Read([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
         {
             List<WeatherForecast> result = new List<WeatherForecast>();
 
             foreach (var value in _holder.Values)
             {
-                if (value.Date <= Convert.ToDateTime(toDate) && value.Date >= Convert.ToDateTime(fromDate))
+                if (value.Date <= toDate && value.Date >= fromDate)
                 {
                     result.Add(value);
                 }
@@ -40,18 +40,18 @@ namespace WebApplication.Controllers
         }
 
         [HttpDelete("delete")]
-        public IActionResult Delete([FromQuery] string fromDate, [FromQuery] string toDate)
+        public IActionResult Delete([FromQuery] DateTime fromDate, [FromQuery] DateTime toDate)
         {
-            _holder.Values = _holder.Values.Where(w => !(w.Date <= Convert.ToDateTime(toDate) && w.Date >= Convert.ToDateTime(fromDate))).ToList();
+            _holder.Values = _holder.Values.Where(w => !(w.Date <= toDate && w.Date >= fromDate)).ToList();
             return Ok();
         }
 
         [HttpPut("update")]
-        public IActionResult Update([FromQuery] string date, [FromQuery] int newValue)
+        public IActionResult Update([FromQuery] DateTime date, [FromQuery] int newValue)
         {
             foreach (var value in _holder.Values)
             {
-                if (value.Date == Convert.ToDateTime(date))
+                if (value.Date == date)
                 {
                     value.TemperatureC = newValue;
                 }
