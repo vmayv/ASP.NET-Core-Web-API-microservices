8bded0fa4228388040d6d278ad06ec0239dc7f00
vmayv <vasyamayorov@yandex.ru>
2021-03-24 16:03:31 +0300

    Добавлены методы с заглушками в контроллеры

--- WebApplication/Controllers/DotNetMetricsController.cs
+++ WebApplication/Controllers/DotNetMetricsController.cs
@@ -11,5 +11,29 @@ namespace WebApplication.Controllers
     [ApiController]
     public class DotNetMetricsController : ControllerBase
     {
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("agent/{agentId}/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetricsFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("cluster/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentileFromAllCluster([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime,
+            [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
     }
 }
