79fac9f7cd188068b544b63e88b207028da49c20
vmayv <vasyamayorov@yandex.ru>
2021-03-25 12:45:57 +0300

    Добавлены контроллеры и их код в проект агента сбора метрик

---
+++ MetricsAgent/Controllers/CpuMetricsController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static ClassLibrary.Class;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/metrics/cpu")]
+    [ApiController]
+    public class CpuMetricsController : ControllerBase
+    {
+        [HttpGet("/from/{fromTime}/to/{toTime}")]
+        public IActionResult GetMetrics([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            return Ok();
+        }
+
+        [HttpGet("/from/{fromTime}/to/{toTime}/percentiles/{percentile}")]
+        public IActionResult GetMetricsByPercentile([FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime, [FromRoute] Percentile percentile)
+        {
+            return Ok();
+        }
+    }
+}
