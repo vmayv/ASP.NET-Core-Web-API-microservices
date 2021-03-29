bac09405f4434bd4514768bc42041abf59369378
vmayv <vasyamayorov@yandex.ru>
2021-03-29 14:33:30 +0300

    Тестирование SQLite

---
+++ MetricsAgent/Controllers/SqlController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Data.SQLite;
+
+namespace MetricsAgent.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SqlController : ControllerBase
+    {
+        [HttpGet("sql-test")]
+        public IActionResult TryToSqlLite()
+        {
+            string connectionString = "Data Source=:memory:";
+            string sqlText = "SELECT SQLITE_VERSION()";
+
+            using (var connection = new SQLiteConnection(connectionString))
+            {
+                connection.Open();
+
+                using var cmd = new SQLiteCommand(sqlText, connection);
+                string version = cmd.ExecuteScalar().ToString();
+
+                return Ok(version);
+            }
+
+        }
+    }
+}
