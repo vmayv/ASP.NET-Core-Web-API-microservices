77c4fe5db090dc95a709933ced12d6b1b86bc478
vmayv <vasyamayorov@yandex.ru>
2021-04-09 14:29:39 +0300

    Добавлен класс MapperProfile

---
+++ MetricsAgent/MapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MetricsAgent.DTO;
+using MetricsAgent.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsAgent
+{
+    public class MapperProfile : Profile
+    {
+        public MapperProfile()
+        {
+            CreateMap<CpuMetric, CpuMetricDto>();
+        }
+
+    }
+}
