eaa9dc5f6ac313c7ff40beddf8ab83a74326b1fc
vasiliy.mayorov <vasyamayorov@yandex.ru>
2021-04-18 19:54:29 +0300

    Добавлены Models

---
+++ MetricsManager/DAL/DateTimeOffsetHandler.cs
@@ -0,0 +1,19 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MetricsManager.DAL
+{
+    public class DateTimeOffsetHandler : SqlMapper.TypeHandler<DateTimeOffset>
+    {
+        public override DateTimeOffset Parse(object value)
+           => new DateTimeOffset((long)value, TimeSpan.FromHours(3));
+
+        public override void SetValue(IDbDataParameter parameter, DateTimeOffset value)
+            => parameter.Value = value;
+
+    }
+}
