5f9c6bf4bfb70113c4da01b68193215783cc2919
vasiliy.mayorov <vasyamayorov@yandex.ru>
2021-04-18 20:05:52 +0300

    Добавлены общие интерфейсы и класс параметров строки подключения к бд

---
+++ Core/Interfaces/IRepositoryApi.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.Interfaces
+{
+    interface IRepositoryApi<T> where T : class
+    {
+        void Create(T item);
+
+        IList<T> GetMetricfromDatabase(int adentId, DateTimeOffset fromTime, DateTimeOffset toTime);
+
+        T GetMetricbyPercentilefromDatabase(int agentId, DateTimeOffset fromTime, DateTimeOffset toTime, Percentile percentile);
+
+        IList<T> GetMetricfromClusterfromDatabase(DateTimeOffset fromTime, DateTimeOffset toTime);
+
+        T GetMetricbyPercentilefromClusterfromDatabase(DateTimeOffset fromTime, DateTimeOffset toTime, Percentile percentile);
+
+        DateTimeOffset GetLastTime(int agentId);
+    }
+}
