f539f993bb72cc89a0a863d77dae27930b88d780
vmayv <vasyamayorov@yandex.ru>
2021-04-23 13:34:31 +0300

    Добавлен проект интерфейса к менеджеру метрик Реализован пример из методички

---
+++ MetricsManagerClient/MaterialCards.xaml.cs
@@ -0,0 +1,58 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MetricsManagerClient
+{
+    /// <summary>
+    /// Логика взаимодействия для MaterialCards.xaml
+    /// </summary>
+    public partial class MaterialCards : UserControl, INotifyPropertyChanged
+    {
+
+        public MaterialCards()
+        {
+            InitializeComponent();
+
+            ColumnServiesValues = new SeriesCollection
+            {
+                new ColumnSeries
+                {
+                    Values = new ChartValues<double> { 10,20,30,40,50,60,70,80,90.100 }
+                }
+            };
+
+            DataContext = this;
+        }
+
+        public SeriesCollection ColumnServiesValues { get; set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged(string propertyName = null)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        private void UpdateOnСlick(object sender, RoutedEventArgs e)
+        {
+            TimePowerChart.Update(true);
+        }
+    }
+
+}
